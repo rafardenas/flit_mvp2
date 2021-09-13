e2e6bbcf68c748ebcacc4e270e4e99589c6b9ec2
Rafael Cardenas <rafardenas@gmail.com>
2021-09-13 10:59:05 -0500

    redeploy

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,7 @@ python manage.py collectstatic --no-input
 
 python manage.py runserver 
 
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
 gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi
