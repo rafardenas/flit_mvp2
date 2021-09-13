d481946188170394e902ff32b7667f671d843bbc
Rafael Cardenas <rafardenas@gmail.com>
2021-09-13 12:07:57 -0500

    redeploy

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,8 @@
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 DJANGO_SETTINGS_MODULE=djcrm.settings 
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=:"${PYTHONPATH}:/Users/Rafa/opt/anaconda3/envs/django/bin"
 
 python3 manage.py migrate 
 
