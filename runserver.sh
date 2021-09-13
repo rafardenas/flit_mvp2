e18ab22b701822b63988b749a1aac7ba9d0a21b1
Rafael Cardenas <rafardenas@gmail.com>
2021-09-13 12:55:28 -0500

    redeploy

--- runserver.sh
+++ runserver.sh
@@ -1,9 +1,5 @@
-python3 manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-DJANGO_SETTINGS_MODULE=djcrm.settings 
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=:"${PYTHONPATH}:/Users/Rafa/opt/anaconda3/envs/django/bin"
+python manage.py migrate
 
-python3 manage.py migrate 
-
-gunicorn --worker-tmp-dir /dev/shm djcrm.application
+gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi 
