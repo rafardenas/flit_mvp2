848452cd722905f38853daa63f9678f3ddc2c986
Rafael Cardenas <56218994+rafardenas@users.noreply.github.com>
2022-07-30 14:28:16 -0500

    Update settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = env('SECRET_KEY')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-m9f5md2*cs*^6ek%--quu67s7fqcglg7j^nxb+pctc&za-o$x+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
